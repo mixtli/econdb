48e1ac39b84735199a029a2cc3533e8921d4a610
Ron McClain <ronmcclain75@gmail.com>
2009-04-15 00:17:24 -0700

    stuff

--- vendor/plugins/graph_engine/app/helpers/data_sources_helper.rb
+++
@@ -1,9 +0,0 @@
-module DataSourcesHelper
-  def display_plugin_arguments(name, resource, options)
-    case options[:type]
-      when 'select_tag' then select_tag "data_source[arguments][#{name}]", options_for_select(options[:options])
-      else eval "#{options[:type]} \"data_source[arguments][#{name}]\", resource.arguments[name]"
-    end
-  end
-
-end
